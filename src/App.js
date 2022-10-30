0a67af9eaa4df7b0631fa433a89b241269b9bc69
Troneci Andrei <troneciandrei@gmail.com>
2022-10-30 21:38:19 +0200

    Advice Generator with Author display

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import Dice from "./images/icon-dice.svg";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [quotes, setQuotes] = useState([]);
+  const [quotes, setQuotes] = useState("");
+  const [author, setAuthor] = useState("");
+  const [number, setNumber] = useState(0);
 
   const fetchAdvice = () => {
-    fetch("https://api.adviceslip.com/advice")
+    fetch("https://type.fit/api/quotes")
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        setQuotes(data.slip);
+        console.log(data);
+
+        let dataQuotes = data;
+
+        let randomNumber = Math.floor(Math.random() * dataQuotes.length);
+        let randomQuote = dataQuotes[randomNumber];
+
+        setQuotes(randomQuote.text);
+        setNumber(randomNumber);
+        setAuthor(randomQuote.author);
       });
   };
 
@@ -22,11 +33,16 @@ function App() {
   return (
     <div className="container mx-auto flex items-center justify-center h-screen">
       <div className="bg-[#313a49] w-[540px] h-[331px] text-white text-center p-6 flex flex-col  justify-center gap-5 items-center rounded-xl">
-        <span className="text-[#52fea8] uppercase font-bold tracking-widest">
-          {`Advice #${quotes.id}`}
+        <span className="text-[#52fea8] uppercase font-bold tracking-widest flex gap-2">
+          <span>{`Advice #${number}`}</span>
+
+          <span>{author ? author : "No Author"}</span>
         </span>
-        <h1 className="font-bold text-xl text-[#cce4e8]">{quotes.advice}</h1>
+
+        <h1 className="font-bold text-xl text-[#cce4e8]">{`"${quotes}"`}</h1>
+
         <img src={Divider} alt="divider" />
+
         <button
           type="button"
           title="Dice new quote"
